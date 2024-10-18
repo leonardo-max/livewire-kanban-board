e5d54529a8adbeecf2a466aa4473f8b42188dde8
Leonardo Max <l3onardomax@hotmail.com>
2024-10-18 15:11:42 -0300

    Update SampleLivewireKanbanBoard.php

--- tests/stubs/SampleLivewireKanbanBoard.php
+++ tests/stubs/SampleLivewireKanbanBoard.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace Mantix\LivewireKanbanBoard\Tests\Stubs;
+namespace leonardo-max\LivewireKanbanBoard\Tests\Stubs;
 
-use Mantix\LivewireKanbanBoard\LivewireKanbanBoard;
+use leonardo-max\LivewireKanbanBoard\LivewireKanbanBoard;
 use Illuminate\Support\Collection;
 
 class SampleLivewireKanbanBoard extends LivewireKanbanBoard {
